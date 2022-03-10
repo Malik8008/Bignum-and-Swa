d522fd9fb2bdb5375961be4f024819735251d60a
Malik8008 <tu60prbs1@code.edu.az>
2022-03-10 11:30:00 +0400

    Search bignum

---
+++ Search big num/Search big num/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Search_big_num
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Arr = { 15,21,9,19,20,2,8 };
+            Console.WriteLine(Bignum(Arr));
+        }
+
+
+        public static int Bignum(int[] Arr) 
+        {
+            int bignum = Arr[4];
+            foreach (var number in Arr)
+            {
+                if (number>bignum)
+                {
+                    bignum=number;
+                }                
+            }
+            return bignum;
+        }
+    }
+}
